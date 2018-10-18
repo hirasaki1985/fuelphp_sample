26ff1c72fb132e1d3709754c33c83e51c970d67c
hirasaki <hirasaki@re-al.co.jp>
2018-10-18 17:55:04 +0900

    db access error bug fix

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,14 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev;charset=utf8;unix_socket=/var/run/mysqld/mysqld.sock',
+			// 'dsn'        => 'mysql:host=localhost;dbname=fuel_dev;charset=utf8;unix_socket=/var/run/mysqld/mysqld.sock',
+			'hostname' => 'localhost',
+			'port'     => '3306',
+			'database' => 'fuel_dev',
 			'username'   => 'root',
 			'password'   => '',
+			'socket' => '/var/run/mysqld/mysqld.sock'
 		),
+		'profiling' => true,
 	),
 );
