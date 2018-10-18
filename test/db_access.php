6b3a7fce3484d775650330e3fe18614d88d47ead
hirasaki <hirasaki@re-al.co.jp>
2018-10-18 17:49:49 +0900

    update test db_access

--- test/db_access.php
+++ test/db_access.php
@@ -1,9 +1,10 @@
 <?php
 $pdo = new PDO ( 'mysql:host=localhost;dbname=fuel_dev;charset=utf8', 'root', '' );
+// $sql = $pdo->prepare ( 'select * from migration' );
 $sql = $pdo->prepare ( 'show tables' );
 $sql->execute();
 foreach ( $sql->fetchAll () as $row ) {
-  echo '$row';
+  print_r($row);
   echo '';
 }
 ?>
